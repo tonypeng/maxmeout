895f1da1eb9cae418bafcea12a7674034ffd8eaa
Tony Peng <me@tonypeng.com>
2020-12-16 01:26:19 -0800

    import code

---
+++ README.md
@@ -0,0 +1,8 @@
+# maxmeout
+Monitor your nearby Apple stores for Airpods Max availability!
+
+### Instructions
+By using this software, you agree to the terms in the `LICENSE` file.
+1. First, you'll need to get the page URL of the store you want to monitor (the script will automatically monitor (up to) the 11 nearest stores to that store as well). To do that, go to the page on the Apple website for your store (you can just google "apple \<location\>") and copy the URL. For example, the Apple Park Visitor Center store page is https://www.apple.com/retail/appleparkvisitorcenter/.
+2. Install the dependencies for the code. There are two: `requests` for fetching inventory data from Apple's website and `beepy` for making a beep sound when inventory is found.<br>`$ pip3 install requests beepy`
+3. Now you're ready to run the script! Download this repository, open a terminal window, and `cd` to where you've downloaded the code. This should be the same directory where `main.py` is. Run the script with:<br>```$ python3 main.py --store_url STORE_URL --models MODELS_TO_WATCH```<br>, replacing `STORE_URL` with the store page url you noted down in step 1, and `MODELS_TO_WATCH` with a comma separated of the list of colors you want to monitor: `space_gray`, `silver`, `green`, `sky_blue`, `pink`. For example, to monitor for space gray, silver, and sky blue availability at Apple Park Visitor Center:<br>`$ python3 main.py --store_url https://www.apple.com/retail/appleparkvisitorcenter/ --models space_gray,silver,sky_blue`<br>At the time of writing, `silver` is in stock at Apple Park Visitor Center. Good luck, and happy hunting!
---
+++ main.py
@@ -0,0 +1,63 @@
+import argparse
+import beepy
+import datetime
+import re
+import requests
+import time
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--store_url', type=str, help='The Apple website page url of the primary store to watch. The 11 nearest stores to the primary store will also be watched.')
+parser.add_argument('--models', type=str, help="A comma separated list of the models to watch. Valid values are space_gray, silver, green, sky_blue, pink.")
+parser.add_argument('--sleep_time', type=float, default=5, help='The number of seconds to wait in between each check. Default: 5 seconds.')
+
+args = parser.parse_args()
+
+if args.store_url is None or args.models is None:
+    exit('--store_url and --models are required')
+
+model_name_to_id = {
+    'space_gray': 'MGYH3AM/A',
+    'silver': 'MGYJ3AM/A',
+    'green': 'MGYN3AM/A',
+    'sky_blue': 'MGYL3AM/A',
+    'pink': 'MGYM3AM/A',
+}
+
+model_id_to_name = {v: k for k, v in model_name_to_id.items()}
+
+r = requests.get(args.store_url)
+if r.status_code != 200:
+    exit('Got an unexpected response from the store page. Did you specify the correct URL?')
+_, store_to_watch = re.search('<meta property=\"analytics-track\" content=\"Retail Store - (.+?):(.+?)\" \/>', r.text).groups()
+models_to_watch = [model_name_to_id[model_id] for model_id in args.models.split(',')]
+sleep_time = args.sleep_time
+
+def log(msg):
+    print(f'[{datetime.datetime.now()}] {msg}')
+
+def check_store(store, product):
+    return store['partsAvailability'][product]['pickupDisplay'] != 'unavailable'
+
+def check_model_near(model, store_id):
+    r = requests.get(
+        f'https://www.apple.com/shop/retail/pickup-message?pl=true&searchNearby=true&store={store_id}&parts.0={model}')
+    if r.status_code == 200:
+        resp = r.json()
+        found = False
+        for store in resp['body']['stores']:
+            store_name = store['storeName']
+            if check_store(store, model):
+                print(f'Found {model_id_to_name[model]} availability at {store_name}!')
+                beepy.beep(1)
+                found = True
+        if not found:
+            log(f'No availability found for {model_id_to_name[model]}.')
+    else:
+        print(f"Received unexpected status code ${r.status_code}")
+
+while True:
+    for model in models_to_watch:
+        check_model_near(model, store_to_watch)
+
+    time.sleep(sleep_time)
